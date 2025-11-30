b258f0fa1179aede4a663b840d0a97dd1576a207
FurrySharks <sbaka8233@gmail.com>
2025-11-30 03:22:44 +0300

    Update yaya.lua

--- yaya.lua
+++ yaya.lua
@@ -23,3 +23,23 @@ if not checkUserID() then
     return
 end
 
+
+local EXPECTED_URL = "https://raw.githubusercontent.com/FurrySharks/furrysharks/refs/heads/main/yaya.lua"
+
+local function checkSource()
+    local success, hash = pcall(getscripthash)
+    if not success then return false end
+    
+    local currentUrl = "https://raw.githubusercontent.com/" .. hash
+    
+    if currentUrl ~= EXPECTED_URL and currentUrl ~= EXPECTED_URL:gsub("refs/heads", "refs/tags") then
+        return false
+    end
+    return true
+end
+
+if not checkSource() then
+    error("haha fuck off skid")
+    -- или просто return / while true do task.wait(9e9) end
+end
+
